4a435e09090e96ecf1c6c2908fe045c687cf9b3a
solevicinity <michaelt.abayneh@gmail.com>
2021-10-02 16:19:02 -0400

    Uuid and nodemon installed with other files created

--- reference/event_demo.js
+++ reference/event_demo.js
@@ -1,1 +1,15 @@
-const EventEmitter = require("events")
+const EventEmitter = require("events");
+
+// Create class
+class MyEmitter extends EventEmitter {}
+
+// Init object
+const myEmitter = new MyEmitter();
+
+// Event listener
+myEmitter.on("event", () => {
+  console.log("Event fired!");
+});
+
+// Init event
+myEmitter.emit("event");
